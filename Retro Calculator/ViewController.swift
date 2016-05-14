804b12ba71e0e19d8d2825b91deab3ae2a50d6a1
Vitaliy <vitaliy.fry@gmail.com>
2016-05-14 14:24:57 -0700

    add operations

--- Retro Calculator/ViewController.swift
+++ Retro Calculator/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    enum Operation: String {
+        case Add       = "+"
+        case Substract = "-"
+        case Multiply  = "*"
+        case Divide    = "/"
+        case Equal     = "="
+        case Empty     = "Empty"
+    }
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    var buttonSound: AVAudioPlayer!
+    var currentOperation: Operation = Operation.Empty
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundUrl = NSURL(fileURLWithPath: path!)
+        
+        do {
+            try buttonSound = AVAudioPlayer(contentsOfURL: soundUrl)
+                buttonSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func numberPressed(button: UIButton!) {
+        buttonSound.play()
     }
-
-
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func onSubstractPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+    }
 }
 
